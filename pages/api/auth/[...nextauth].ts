4d6c72285cc2e9a4190d509f277528b2a5226092
Tung Kai Sheng <kai.s.tung@gmail.com>
2021-02-06 17:12:16 +1000

    Setup next-auth & prisma

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import Adapters from 'next-auth/adapters'
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+//initialize prisma
+const prisma = new PrismaClient()
+
+export default (req : NextApiRequest, res : NextApiResponse) => NextAuth(req, res, {
+  providers: [
+    Providers.GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+      })
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import {signIn, signOut, useSession} from 'next-auth/client'
+
 export default function Home() {
+  const [session, loading] = useSession()
+
+  if (session){
+    return (
+      <div><div>{JSON.stringify(session, null, 2)}</div><div><button onClick={() => signOut()}>Logout</button></div></div>
+    )
+  }
   return (
     <div>
       Home
+      <button onClick={() => signIn('github')}>Login</button>
     </div>
   )
 }
