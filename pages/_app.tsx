8017946ec23b50e5f11dd1c2466e0f3a6112e0d4
Tung Kai Sheng <kai.s.tung@gmail.com>
2021-02-11 14:07:29 +1000

    refactor: Change AuthError to CustomError

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import { AuthError } from "auth/error";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import Error from "pages/_error";
+import { CustomError } from "types";
 
 interface Props extends AppProps {}
 
 const MyApp: NextPage<Props> = ({ Component, pageProps }) => {
-  const error = pageProps.error as AuthError;
+  const error = pageProps.error as CustomError;
   if (error) {
     return <Error error={error} />;
   }
